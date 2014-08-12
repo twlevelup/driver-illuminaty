2197a88a2d9c1dc1e6ff4d7624b543565d1d9df9
Rene Vallenilla <renevp@gmail.com>
2014-08-12 20:27:03 +1000

    New Taxi class and test cases fixed.

--- spec/taxi_spec.rb
+++ spec/taxi_spec.rb
@@ -2,27 +2,27 @@ require "taxi"
 
 describe Taxi do
     it 'move one position to north' do
-      t = Taxi.new 1, 1,"N" #arrange
-      position = t.move  #act
+      current_taxi = Taxi.new 1, 1,"N" #arrange
+      position = current_taxi.move  #act
       expect(position).to eq("1,2,N") #assert
     end
     
     it 'move one position to south' do
-      t = Taxi.new 1, 1,"S" #arrange
-      position = t.move  #act
+      current_taxi = Taxi.new 1, 1,"S" #arrange
+      position = current_taxi.move  #act
       expect(position).to eq("1,0,S") #assert
     end
     
     it 'move one position to east' do
-      t = Taxi.new 1, 1,"E" #arrange
-      position = t.move  #act
+      current_taxi = Taxi.new 1, 1,"E" #arrange
+      position = current_taxi.move  #act
       expect(position).to eq("2,1,E") #assert
     end
     
     it 'move one position to west' do
-      t = Taxi.new 1, 1,"W" #arrange
-      position = t.move  #act
-      expect(position).to eq("0,1,k") #assert
+      current_taxi = Taxi.new 1, 1,"W" #arrange
+      position = current_taxi.move  #act
+      expect(position).to eq("0,1,W") #assert
     end
 
 end
