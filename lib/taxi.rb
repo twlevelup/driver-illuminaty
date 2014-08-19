d017a575d810ad9dde58dfa48c6618b90fdea614
Andrew G <andrew.giannopoulos@gmail.com>
2014-08-19 19:00:30 +1000

    added location method to retrieve taxi current position

--- lib/taxi.rb
+++ lib/taxi.rb
@@ -3,6 +3,8 @@ class Taxi
     @x = x
     @y = y
     @direction = direction
+    @xDest = 0
+    @yDest = 0
   end
 
   def move
@@ -15,6 +17,16 @@ class Taxi
 	elsif @direction == "W"
 	  @x -= 1 
 	end
+
+	if @x == @xDest && @y == @yDest
+		
+	end
+
 	@x.to_s + "," + @y.to_s + "," + @direction
   end
+
+  def location
+  	@x.to_s + ", " + @y.to_s
+  end
+
 end
