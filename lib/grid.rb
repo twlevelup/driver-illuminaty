601f7518c44e4073f99c9bc6de716f3388f40d6b
Rene Vallenilla <renevp@gmail.com>
2014-09-02 20:10:04 +1000

    Grid boundaries implemented.

--- lib/grid.rb
+++ lib/grid.rb
@@ -1,8 +1,22 @@
 class Grid
-	attr_accessor :x, :y
+	attr_accessor :x, :y, :taxis
 	
 	def initialize(x, y)
 		@x = x
 		@y = y
+		@taxis = []
 	end
+	
+	def add_taxi(taxi)
+		@taxis << taxi
+	end
+	
+	def taxi_outside?(taxi)
+		if (taxi.x > @x) || (taxi.y > @y) || (taxi.x < 0) || (taxi.y < 0)
+			true
+		else
+			false
+		end
+	end
+	
 end
--- spec/grid_spec.rb
+++ spec/grid_spec.rb
@@ -7,4 +7,55 @@ describe Grid do
 		expect(current_grid.y).to eq(5)#assert
 	end
 	
+	it 'should have a least a taxi' do	
+		current_grid = Grid.new 9, 5  #arrange
+		taxi = Taxi.new 1, 1,"N" #arrange
+		current_grid.add_taxi taxi #act
+		expect(current_grid.taxis.size).to eq(1)#assert
+	end
+	
+	it 'should be outside the grid' do	
+		current_grid = Grid.new 9, 5  #arrange
+		taxi = Taxi.new 9, 5,"N" #arrange
+		taxi.move #act
+		current_grid.add_taxi taxi #act
+		is_outside = current_grid.taxi_outside? taxi
+		expect(is_outside).to eq(true)#assert
+	end
+	
+	it 'should be inside the grid' do	
+		current_grid = Grid.new 9, 5  #arrange
+		taxi = Taxi.new 8, 4,"N" #arrange
+		taxi.move #act
+		current_grid.add_taxi taxi #act
+		is_outside = current_grid.taxi_outside? taxi
+		expect(is_outside).to eq(false)#assert
+	end
+	
+	it 'should be inside the grid 2' do	
+		current_grid = Grid.new 9, 5  #arrange
+		taxi = Taxi.new 9, 5,"S" #arrange
+		taxi.move #act
+		current_grid.add_taxi taxi #act
+		is_outside = current_grid.taxi_outside? taxi
+		expect(is_outside).to eq(false)#assert
+	end
+	
+	it 'should be inside the grid 2' do	
+		current_grid = Grid.new 9, 5  #arrange
+		taxi = Taxi.new 9, 5,"S" #arrange
+		taxi.move #act
+		current_grid.add_taxi taxi #act
+		is_outside = current_grid.taxi_outside? taxi
+		expect(is_outside).to eq(false)#assert
+	end
+	
+	it 'should be ouside the grid with negatives values' do	
+		current_grid = Grid.new 9, 5  #arrange
+		taxi = Taxi.new 0, 0,"S" #arrange
+		taxi.move #act
+		current_grid.add_taxi taxi #act
+		is_outside = current_grid.taxi_outside? taxi
+		expect(is_outside).to eq(true)#assert
+	end
 end
